bb3a2ff47342abca813544fdce0fcd61674059e2
tomas-schmidt <tomi_95@hotmail.com>
2016-08-15 05:48:01 -0300

    agrego lo que faltaba de bajaDePois

--- tp disenio/tp_disenio_1/tp_disenio_1/Comandos/BajaDePois.cs
+++ tp disenio/tp_disenio_1/tp_disenio_1/Comandos/BajaDePois.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace tp_disenio_1.Comandos
 {
-    class BajaDePois
+    class BajaDePois: Comando
     {
+        private DateTime fechaDeBaja;
+        private string valorBusqueda;
+
+        public override void ejecutar()
+        {
+            fechaInicio = fechaDeBaja;
+
+            try
+            {
+                resultado = true;
+                List<Poi> poisAEliminar = new List<Poi>();
+
+                CatalogoPois catalogo = CatalogoPois.Instance();
+
+                poisAEliminar = catalogo.buscar(valorBusqueda);
+
+
+                foreach(Poi poi in poisAEliminar)
+                {
+                    catalogo.eliminarPoi(poi);
+                }
+            }
+
+            catch
+            {
+                resultado = false;
+            }
+
+            fechaFin = DateTime.Now;
+            this.guardar(); ;
+        }
+
+        public override void undo()
+        {
+        }
     }
 }
