a1c0a91c08fefe7336db1150cb5b9b22babe8274
tomas-schmidt <tomi_95@hotmail.com>
2016-10-03 22:29:32 -0300

    agrego unos detalles

--- tp disenio/tp_disenio_1/tp_disenio_1/ABM Pois/AbmPois.Designer.cs
+++ tp disenio/tp_disenio_1/tp_disenio_1/ABM Pois/AbmPois.Designer.cs
@@ -40,7 +40,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(143, 51);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Alta POI";
+            this.button1.Text = "Crear nuevo POI";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -52,7 +52,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(143, 52);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Modificar/Baja POI";
+            this.button2.Text = "Alta/Baja POI";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
